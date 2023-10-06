f13b982b2effc385966a1b4fc6d9f878261fbb2a
shiftroll <shiftroll@outlook.com>
2023-10-06 10:59:01 +0700

    commit

---
+++ .webflowrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: "https://api.webflow.com",
+    rootDir: "./devlink",
+    siteId: "63fc0698f87ebe53ab32acc8",
+    authToken: "d9844d86db9bd85393da2e56da1da018cb6a02090fc9f3fe372e88c23d2e65be",
+  }
